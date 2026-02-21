d0e32cb839b6c0935764a8ac3f0a0caf9b1ef6b9
nishant chaurasiya <nishantchaurasiya93@gmail.com>
2026-02-21 19:28:32 +0530

    day-7-completed

---
+++ day-7/frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import "./app.css"
+import axios from "axios"
+const App = () => {
+  const [notesData, setNotesData] = useState([])
+  axios.get("http://localhost:3000/notes")
+  .then((res)=>{
+   setNotesData(res.data.notes);
+   
+  })
+  return (
+    <main>
+    {notesData.map((note,ind)=>( 
+     <div className="note" key={ind}>
+      <p><span>title:</span>{note.title}</p>
+      <p><span>description:</span>{note.description}</p>
+     </div>
+    ))}
+    </main>
+  )
+}
+
+export default App 
